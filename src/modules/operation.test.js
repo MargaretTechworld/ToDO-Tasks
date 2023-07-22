b041800d400c514d6c66920c846eb33d8a7459cb
MargaretTechworld <margarettechworld@gmail.com>
2023-07-22 13:45:00 +0000

    add update test

--- src/modules/operation.test.js
+++ src/modules/operation.test.js
@@ -9,4 +9,15 @@ describe('Manipulate status and content updates', () => {
     todo.clearAll();
     expect(todo.tasks).toHaveLength(0);
   });
+
+  test('Editing task Description', () => {
+    todo.addTask('Test 3');
+    todo.editTask(0, 'Test 4');
+    expect(todo.tasks[0]?.description).toBe('Test 4');
+  });
+  
+  test('Check task status', () => {
+    todo.complete(0, true);
+    expect(todo.tasks.at(0).completed).toBe(true);
+  });
 });
