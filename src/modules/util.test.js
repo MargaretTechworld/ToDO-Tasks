b98eb7851d892e702afa85b833653dc9e7c3d317
yusufyusie <yesuffenta023@gmail.com>
2023-07-22 10:55:20 +0300

    Add a test for remove functionality

--- src/modules/util.test.js
+++ src/modules/util.test.js
@@ -12,4 +12,21 @@ describe('add and remove functions', () => {
     expect(tasks).toHaveLength(1)
   })
   
+  test('remove', () => {
+    const toDoList = [{
+      description: 'sleep',
+      completed: false,
+      index: 1
+    }]
+    localStorage.setItem('tasks', JSON.stringify(toDoList))
+    document.body.innerHTML = `
+      <div class='todo-list'>
+        <div class="task">
+        </div>
+      </div>
+    `
+    deleteTask(toDoList[0].index)
+    const tasks = document.querySelectorAll('.todo-list .task');
+    expect(tasks).toHaveLength(0)
+  })
 })
